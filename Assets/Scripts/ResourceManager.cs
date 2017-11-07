89e1909e89e2cff8e9cb2f684a521693db22b277
Sergio Viudes <sjvc@users.noreply.github.com>
2017-11-07 08:40:23 +0100

    Update ResourceManager.cs

--- Assets/Scripts/ResourceManager.cs
+++ Assets/Scripts/ResourceManager.cs
@@ -1,6 +1,7 @@
 using UnityEngine;
 using System.Collections.Generic;
 using System;
+using System.IO;
 using System.Xml;
 using System.Text.RegularExpressions;
 
@@ -9,51 +10,113 @@ using System.Text.RegularExpressions;
 	so strings are loaded as fast as possible
 **/
 
-/// <summary>
-/// For text resources, it expects Android xml values file format
-/// </summary>
 public class ResourceManager : MonoBehaviour {
 
 	[HideInInspector]
-	public static ResourceManager instance;
+	public static StringResourceManager stringResources;
+
+	void Awake(){
+		stringResources = new StringResourceManager();
+
+		stringResources.LoadStrings(null); // Load default language strings
+		stringResources.LoadStrings(StringResourceManager.GetISOCodeFromSystemLanguage()); // Override system language existing strings
+	}
+
+	public static string[] GetStringKeys(){
+		return stringResources.GetStringKeys();
+	}
+
+	public static string GetString(string key){
+		return stringResources.GetString(key);
+	}
+
+	public static string GetString(string key, object arg0){
+		return stringResources.GetString(key, arg0);
+	}
+
+	public static string GetString(string key, object arg0, object arg1){
+		return stringResources.GetString(key, arg0, arg1);
+	}
+
+	public static string GetString(string key, object arg0, object arg1, object arg2){
+		return stringResources.GetString(key, arg0, arg1, arg2);
+	}
+
+	public static string GetString(string key, params object[] args){
+		return stringResources.GetString(key, args);
+	}
+}
+
+/// <summary>
+/// For text resources, it expects Android xml values file format
+/// </summary>
+public class StringResourceManager {
 	private static Regex placeHoldersRegEx = new Regex(@"%(\d+)\$.");
 	private static string[] placeHolderGroups = new string[20];
 	
-	protected Dictionary<string, string> strings = new Dictionary<string, string>();
-	
-	void Awake(){
-		instance = this;
+	private Dictionary<string, string> strings = new Dictionary<string, string>();
+	private string[] stringKeys;
+	private List<string> providedIsoCodes;
 
+	public StringResourceManager(){
 		for (int i=0; i<placeHolderGroups.Length; i++){
 			placeHolderGroups[i] = string.Format("{{0}}", i);
 		}
-
-		LoadStrings(null); // Load default language strings
-		LoadStrings(GetISOCodeFromSystemLanguage()); // Override system language existing strings
 	}
 
-	public void LoadStrings(string lang){
-		TextAsset stringsFileAsset = Resources.Load<TextAsset>("strings" + (String.IsNullOrEmpty(lang) ? "" : ("-" + lang)));
+	public void LoadStrings(string langIsoCode){
+		TextAsset stringsFileAsset = Resources.Load<TextAsset>(string.Format("strings{0}", String.IsNullOrEmpty(langIsoCode) ? "" : string.Format("-{0}", langIsoCode)));
 
 		if (stringsFileAsset != null){
 			XmlDocument xmlStringsDoc = new XmlDocument();
 			xmlStringsDoc.LoadXml(stringsFileAsset.text);
 
+			stringKeys = new string[xmlStringsDoc.DocumentElement.ChildNodes.Count];
 			for(int i=0, size=xmlStringsDoc.DocumentElement.ChildNodes.Count; i<size; i++){
 				XmlNode xmlNode = xmlStringsDoc.DocumentElement.ChildNodes[i];
 				strings[xmlNode.Attributes["name"].Value] = Regex.Unescape(xmlNode.InnerText);
+				stringKeys[i] = xmlNode.Attributes["name"].Value;
 			}
 		}
 	}
 
-	public static string GetString(string key){
+	public void ClearStrings(){
+		strings.Clear();
+		stringKeys = null;
+	}
+
+	public string[] GetStringKeys(){
+		return stringKeys;
+	}
+
+	public bool ContainsString(string key){
+		return strings.ContainsKey(key);
+	}
+
+	public string GetString(string key){
 		string str;
-		if (instance.strings.TryGetValue(key, out str)){
+		if (strings.TryGetValue(key, out str)){
 			return ConvertPlaceHolders(str);
 		}
 
-		Logger.WriteWarning("Strings array doesn't contain the key: " + key);
+		Logger.WriteWarning(string.Format("Strings array doesn't contain the key: {0}", key));
 		return "[" + key + "]";
+	}
+
+	public string GetString(string key, object arg0){
+		return string.Format (GetString(key), arg0);
+	}
+
+	public string GetString(string key, object arg0, object arg1){
+		return string.Format (GetString(key), arg0, arg1);
+	}
+
+	public string GetString(string key, object arg0, object arg1, object arg2){
+		return string.Format (GetString(key), arg0, arg1, arg2);
+	}
+
+	public string GetString(string key, params object[] args){
+		return string.Format(GetString(key), args);
 	}
 
 	/// <summary>
@@ -63,71 +126,79 @@ public class ResourceManager : MonoBehaviour {
 		return placeHoldersRegEx.Replace(str, m => placeHolderGroups[ int.Parse(m.Groups[1].Value) - 1 ]);
 	}
 
-	public static string GetString(string key, object arg0){
-		return string.Format (GetString(key), arg0);
-	}
+	public List<string> GetProvidedIsoCodes(){
+		if (providedIsoCodes != null){
+			return providedIsoCodes;
+		}
 
-	public static string GetString(string key, object arg0, object arg1){
-		return string.Format (GetString(key), arg0, arg1);
-	}
+		providedIsoCodes = new List<string>();
+		providedIsoCodes.Add(""); // Default language
+		Array allLanguages = Enum.GetValues(typeof(SystemLanguage));
+		for (int i=0, size=allLanguages.Length; i<size; i++){
+			string langIsoCode = GetISOCodeFromLanguage( (SystemLanguage)allLanguages.GetValue(i) );
+			if (!providedIsoCodes.Contains(langIsoCode)){ // Avoid duplicate entries
+				TextAsset stringsFileAsset = Resources.Load<TextAsset>(string.Format("strings-{0}", langIsoCode));
+				if (stringsFileAsset != null){
+					providedIsoCodes.Add( langIsoCode );
+				}
+			}
+		}
 
-	public static string GetString(string key, object arg0, object arg1, object arg2){
-		return string.Format (GetString(key), arg0, arg1, arg2);
-	}
-
-	public static string GetString(string key, params object[] args){
-		return string.Format(GetString(key), args);
+		return providedIsoCodes;
 	}
 
 	public static string GetISOCodeFromSystemLanguage() {
-		SystemLanguage lang = Application.systemLanguage;
-		string res = "en";
-		switch (lang) {
-			case SystemLanguage.Afrikaans: res = "af"; break;
-			case SystemLanguage.Arabic: res = "ar"; break;
-			case SystemLanguage.Basque: res = "eu"; break;
-			case SystemLanguage.Belarusian: res = "by"; break;
-			case SystemLanguage.Bulgarian: res = "bg"; break;
-			case SystemLanguage.Catalan: res = "ca"; break;
-			case SystemLanguage.Chinese: res = "zh_CN"; break;
-			case SystemLanguage.ChineseSimplified: res = "zh_CN"; break;
-			case SystemLanguage.ChineseTraditional: res = "zh_TW"; break;
-			case SystemLanguage.Czech: res = "cs"; break;
-			case SystemLanguage.Danish: res = "da"; break;
-			case SystemLanguage.Dutch: res = "nl"; break;
-			case SystemLanguage.English: res = "en"; break;
-			case SystemLanguage.Estonian: res = "et"; break;
-			case SystemLanguage.Faroese: res = "fo"; break;
-			case SystemLanguage.Finnish: res = "fi"; break;
-			case SystemLanguage.French: res = "fr"; break;
-			case SystemLanguage.German: res = "de"; break;
-			case SystemLanguage.Greek: res = "el"; break;
-			case SystemLanguage.Hebrew: res = "iw"; break;
-			case SystemLanguage.Hungarian: res = "hu"; break;
-			case SystemLanguage.Icelandic: res = "is"; break;
-			case SystemLanguage.Indonesian: res = "in"; break;
-			case SystemLanguage.Italian: res = "it"; break;
-			case SystemLanguage.Japanese: res = "ja"; break;
-			case SystemLanguage.Korean: res = "ko"; break;
-			case SystemLanguage.Latvian: res = "lv"; break;
-			case SystemLanguage.Lithuanian: res = "lt"; break;
-			case SystemLanguage.Norwegian: res = "no"; break;
-			case SystemLanguage.Polish: res = "pl"; break;
-			case SystemLanguage.Portuguese: res = "pt"; break;
-			case SystemLanguage.Romanian: res = "ro"; break;
-			case SystemLanguage.Russian: res = "ru"; break;
-			case SystemLanguage.SerboCroatian: res = "sh"; break;
-			case SystemLanguage.Slovak: res = "sk"; break;
-			case SystemLanguage.Slovenian: res = "sl"; break;
-			case SystemLanguage.Spanish: res = "es"; break;
-			case SystemLanguage.Swedish: res = "sv"; break;
-			case SystemLanguage.Thai: res = "th"; break;
-			case SystemLanguage.Turkish: res = "tr"; break;
-			case SystemLanguage.Ukrainian: res = "uk"; break;
-			case SystemLanguage.Unknown: res = "en"; break;
-			case SystemLanguage.Vietnamese: res = "vi"; break;
+		return GetISOCodeFromLanguage(Application.systemLanguage);
+	}
+
+	public static string GetISOCodeFromLanguage(SystemLanguage language){
+		string code = "en";
+		switch (language) {
+			case SystemLanguage.Afrikaans: code = "af"; break;
+			case SystemLanguage.Arabic: code = "ar"; break;
+			case SystemLanguage.Basque: code = "eu"; break;
+			case SystemLanguage.Belarusian: code = "by"; break;
+			case SystemLanguage.Bulgarian: code = "bg"; break;
+			case SystemLanguage.Catalan: code = "ca"; break;
+			case SystemLanguage.Chinese: code = "zh_CN"; break;
+			case SystemLanguage.ChineseSimplified: code = "zh_CN"; break;
+			case SystemLanguage.ChineseTraditional: code = "zh_TW"; break;
+			case SystemLanguage.Czech: code = "cs"; break;
+			case SystemLanguage.Danish: code = "da"; break;
+			case SystemLanguage.Dutch: code = "nl"; break;
+			case SystemLanguage.English: code = "en"; break;
+			case SystemLanguage.Estonian: code = "et"; break;
+			case SystemLanguage.Faroese: code = "fo"; break;
+			case SystemLanguage.Finnish: code = "fi"; break;
+			case SystemLanguage.French: code = "fr"; break;
+			case SystemLanguage.German: code = "de"; break;
+			case SystemLanguage.Greek: code = "el"; break;
+			case SystemLanguage.Hebrew: code = "iw"; break;
+			case SystemLanguage.Hungarian: code = "hu"; break;
+			case SystemLanguage.Icelandic: code = "is"; break;
+			case SystemLanguage.Indonesian: code = "in"; break;
+			case SystemLanguage.Italian: code = "it"; break;
+			case SystemLanguage.Japanese: code = "ja"; break;
+			case SystemLanguage.Korean: code = "ko"; break;
+			case SystemLanguage.Latvian: code = "lv"; break;
+			case SystemLanguage.Lithuanian: code = "lt"; break;
+			case SystemLanguage.Norwegian: code = "no"; break;
+			case SystemLanguage.Polish: code = "pl"; break;
+			case SystemLanguage.Portuguese: code = "pt"; break;
+			case SystemLanguage.Romanian: code = "ro"; break;
+			case SystemLanguage.Russian: code = "ru"; break;
+			case SystemLanguage.SerboCroatian: code = "sh"; break;
+			case SystemLanguage.Slovak: code = "sk"; break;
+			case SystemLanguage.Slovenian: code = "sl"; break;
+			case SystemLanguage.Spanish: code = "es"; break;
+			case SystemLanguage.Swedish: code = "sv"; break;
+			case SystemLanguage.Thai: code = "th"; break;
+			case SystemLanguage.Turkish: code = "tr"; break;
+			case SystemLanguage.Ukrainian: code = "uk"; break;
+			case SystemLanguage.Unknown: code = "en"; break;
+			case SystemLanguage.Vietnamese: code = "vi"; break;
 		}
 
-		return res;
+		return code;
 	}
 }
