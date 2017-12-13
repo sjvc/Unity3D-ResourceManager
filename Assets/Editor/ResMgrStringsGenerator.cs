4933a62c5f645c808a6c3796c4c96d0581756964
Sergio Viudes <sjvc@users.noreply.github.com>
2017-12-13 09:49:40 +0100

    Update ResMgrStringsGenerator.cs

--- Assets/Editor/ResMgrStringsGenerator.cs
+++ Assets/Editor/ResMgrStringsGenerator.cs
@@ -7,12 +7,13 @@ using System.Xml;
 
 public class ResMgrStringsGenerator : AssetPostprocessor {
 
-	private const string STRINGS_XML_PATH_REGEXP = @"^Assets/Resources/strings.xml$";
+	private const string STRINGS_XML_PATH_REGEXP = @"^Assets/Resources/Strings/strings.xml$";
+	private const string COMPOSITES_XML_PATH_REGEXP = @"^Assets/Resources/Strings/composites.xml$";
 	private const string STRINGS_CS_PATH = "Scripts/Strings.cs";
 
 	static void OnPostprocessAllAssets(string[] importedAssets, string[] deletedAssets, string[] movedAssets, string[] movedFromAssetPaths){
 		foreach (string asset in importedAssets){
-			if (Regex.IsMatch(asset, STRINGS_XML_PATH_REGEXP)){
+			if (Regex.IsMatch(asset, STRINGS_XML_PATH_REGEXP) || Regex.IsMatch(asset, COMPOSITES_XML_PATH_REGEXP)){
 				UpdateStringsCsFile();
 				return;
 			}
@@ -21,26 +22,33 @@ public class ResMgrStringsGenerator : AssetPostprocessor {
 
 	[MenuItem("Assets/Generate Strings.cs file", false, 10000)]
 	public static void UpdateStringsCsFile(){
-		TextAsset stringsFileAsset = Resources.Load<TextAsset>("strings");
+		string fileContent = "public static class Strings {\n\n";
+		fileContent += GetStringVars("Strings/strings");
+		fileContent += GetStringVars("Strings/composites");
+		fileContent += "\n}";
+
+		File.WriteAllText( Path.Combine(Application.dataPath, STRINGS_CS_PATH), fileContent );
+
+		Debug.Log(STRINGS_CS_PATH + " file updated");
+	}
+
+	private static string GetStringVars(string xmlFilePath){
+		string output = "";
+
+		TextAsset stringsFileAsset = Resources.Load<TextAsset>(xmlFilePath);
 
 		if (stringsFileAsset != null){
 			XmlDocument xmlStringsDoc = new XmlDocument();
 			xmlStringsDoc.LoadXml(stringsFileAsset.text);
 
-			string fileContents = "public static class Strings {\n\n";
-
 			for(int i=0, size=xmlStringsDoc.DocumentElement.ChildNodes.Count; i<size; i++){
 				XmlNode xmlNode = xmlStringsDoc.DocumentElement.ChildNodes[i];
 				string stringKey = xmlNode.Attributes["name"].Value;
-				fileContents += "\tpublic static string " + Camelize(stringKey) + " = \"" + stringKey + "\";\n";
+				output += "\tpublic static string " + Camelize(stringKey) + " = \"" + stringKey + "\";\n";
 			}
-
-			fileContents += "\n}";
-
-			File.WriteAllText( Path.Combine(Application.dataPath, STRINGS_CS_PATH), fileContents );
-
-			Debug.Log(STRINGS_CS_PATH + " file updated");
 		}
+
+		return output;
 	}
 
 	private static string Camelize(string str){
